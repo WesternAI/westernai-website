e4e2d262746bff92a9ef6aa91f3aa63ac01171a6
James Yang <jamesyang663@gmail.com>
2024-02-28 16:14:23 -0500

    minor changes

--- src/pages/components/DarkModeButton.jsx
+++ src/pages/components/DarkModeButton.jsx
@@ -4,20 +4,30 @@ import {switchTheme, isThemeDark} from "../pagejs/switchTheme";
 import Moon from '../../assets/moon.png'
 import Sun from '../../assets/sun.png'
 
+const switchButtonTheme = (theme) => {
+    localStorage.setItem('wai-theme', theme)
+    const img = document.getElementById('dark-mode-img')
+    if(theme === 0){
+        img.src = Moon
+        img.style.filter = 'invert(1)'
+    } else {
+        img.src = Sun
+        img.style.filter = 'invert(0)'
+    }
+}
+
+const handleClick = () => {
+    switchTheme()
+    if(isThemeDark()){
+        switchButtonTheme(0)
+    } else {
+        switchButtonTheme(1)
+    }
+
+}
+
 window.addEventListener('load', () => {
     const darkModeButton = document.getElementById('dark-mode-button')
-
-    const switchButtonTheme = (theme) => {
-        localStorage.setItem('wai-theme', theme)
-        const img = document.getElementById('dark-mode-img')
-        if(theme === 0){
-            img.src = Moon
-            img.style.filter = 'invert(1)'
-        } else {
-            img.src = Sun
-            img.style.filter = 'invert(0)'
-        }
-    }
 
     const initTheme = () => {
         const savedTheme = localStorage.getItem('wai-theme')
@@ -32,20 +42,11 @@ window.addEventListener('load', () => {
     }
 
     initTheme()
-
-    darkModeButton.addEventListener('click', () => {
-        switchTheme()
-        if(isThemeDark()){
-            switchButtonTheme(0)
-        } else {
-            switchButtonTheme(1)
-        }
-    })
 })
 
 const DarkModeButton = () => {
     return (
-        <div id='dark-mode-button'>
+        <div id='dark-mode-button' onClick={handleClick}>
             <img src={Moon} id='dark-mode-img' />
         </div>
     )
