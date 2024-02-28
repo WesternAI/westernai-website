b1fcbf11c4fa3adb775c489d38b503ac7d16dcdd
James Yang <jamesyang663@gmail.com>
2024-02-28 16:40:04 -0500

    portfolio descriptions updated

--- src/pages/pagejs/PORTFOLIO_INFO.js
+++ src/pages/pagejs/PORTFOLIO_INFO.js
@@ -21,10 +21,34 @@ const PORTFOLIO_INFO = [
         description: 'Co-presidents are responsible for steering the overall vision of Western AI and its members. Co-presidents oversee all portfolios and tie the ideals of all the VPs in respective portfolios.',
     },
     {
+        name: 'Flagship',
+        vps: ['Brian Zhou', 'Joy Zhao'],
+        img: flag,
+        description: 'The Flagship portfolio will be responsible for hosting two of the LARGEST AI events at Western this year (Sept/Oct and Jan/Feb), in addition to a few smaller-scale ones. Flagship is responsible for the creative direction and planning of AI case competitions, hackathons, workshops, and many more. Its main goal will be to plan innovative, large-scale events that cater to technical and non-technical students within the community.',        
+    },
+    {
         name: 'Communications',
         vps: ['James Yang'],
         img: comm,        
         description: "The communication portfolio maintain WAI's social media and announcing updates, event opportunities to members. The communication portfolio is also responsible of responding to inquiries from students and forwarding them to appropriate portfolios.",
+    },
+    {
+        name: "Projects",
+        vps: ["Pratik Gupta", 'Shiven Sharma'],
+        img: proj,        
+        description: 'The Projects portfolio provides opportunities for students to build and deploy Machine Learning models that will be presented to industry professionals at major conferences (i.e. CUCAI).',
+    },
+    {
+        name: 'Design',
+        vps: ["Manav Sharma", "Kevin Manka"],
+        img: design,
+        description: 'The Design Portfolio is responsible for developing Western AI’s brand image and leading all creative initiatives within the club. Co-VP Design guides Western AI’s unique brand image and create innovative designs that market our goals, events, and opportunities to up to 800+ individuals.',
+    },
+    {
+        name: 'Development',
+        vps: ['Owen Ostler', 'Lecia Cheng'],
+        img: dev,
+        description: 'The Development Portfolio is focused on introducing all WAI members to employment in AI and tech, while broadening their knowledge of the industry. The Development Portfolio is also editing and redacting Wavelength, Western AI’s own newsletter, and the Speaker Series podcast, alongside with WAI Publications and WAI Flagship.',
     },
     {
         name: 'Publications',
@@ -50,30 +74,6 @@ const PORTFOLIO_INFO = [
         img: edu,
         description: ' The Education portfolio is responsible for helping to provide the Western AI community with educational resources and organizing events/workshops that will help members build their skills and knowledge in the field of Artificial Intelligence.',
     },
-    {
-        name: 'Design',
-        vps: ["Manav Sharma", "Kevin Manka"],
-        img: design,
-        description: 'The Design Portfolio is responsible for developing Western AI’s brand image and leading all creative initiatives within the club. Co-VP Design guides Western AI’s unique brand image and create innovative designs that market our goals, events, and opportunities to up to 800+ individuals.',
-    },
-    {
-        name: 'Development',
-        vps: ['Owen Ostler', 'Lecia Cheng'],
-        img: dev,
-        description: 'The Development Portfolio is focused on introducing all WAI members to employment in AI and tech, while broadening their knowledge of the industry. The Development Portfolio is also editing and redacting Wavelength, Western AI’s own newsletter, and the Speaker Series podcast, alongside with WAI Publications and WAI Flagship.',
-    },
-    {
-        name: 'Flagship',
-        vps: ['Brian Zhou', 'Joy Zhao'],
-        img: flag,
-        description: 'The Flagship portfolio will be responsible for hosting two of the LARGEST AI events at Western this year (Sept/Oct and Jan/Feb), in addition to a few smaller-scale ones. Flagship is responsible for the creative direction and planning of AI case competitions, hackathons, workshops, and many more. Its main goal will be to plan innovative, large-scale events that cater to technical and non-technical students within the community.',        
-    },
-    {
-        name: "Projects",
-        vps: ["Pratik Gupta", 'Shiven Sharma'],
-        img: proj,        
-        description: 'The Projects portfolio provides opportunities for students to build and deploy Machine Learning models that will be presented to industry professionals at major conferences (i.e. CUCAI).',
-    }
 ]
 
 export default PORTFOLIO_INFO;
