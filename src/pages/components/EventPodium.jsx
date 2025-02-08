e9254044f9da6f3a3140b348b8b6ade061eadb16
johnathantam00 <348690827@gapps.yrdsb.ca>
2025-02-07 20:40:15 -0500

    Added stuff

---
+++ src/pages/components/EventPodium.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import "./css/EventPodium.css";
+
+// param {names} -> string array of size 3
+// 
+
+
+const ImageGalleryColumn = ({names, images}) => {
+    // const [firstPlaceName] = useState(names[0]); 
+
+    return (
+      <></>
+    )
+}
